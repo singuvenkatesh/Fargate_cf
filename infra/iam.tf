824f895c14983d954062a88d17e61a9a868fa2d8
singuvenkatesh <singuvenkatesh@gmail.com>
2021-06-30 21:03:56 +0530

    Update iam.tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "role-name"
-
+ 
   assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
@@ -20,7 +20,7 @@ EOF
 
 resource "aws_iam_role" "ecs_task_role" {
   name = "role-name-task"
-
+ 
   assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
@@ -37,7 +37,7 @@ resource "aws_iam_role" "ecs_task_role" {
 }
 EOF
 }
-
+ 
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
